dd8fc8b69e7dc0272b1105ebd3e3681bd543a73b
王欢 <18501667737@163.com>
2021-05-19 19:24:57 +0800

    init

---
+++ src/test/java/com/uwjx/micro/service/sso/MicroServiceSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwjx.micro.service.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
